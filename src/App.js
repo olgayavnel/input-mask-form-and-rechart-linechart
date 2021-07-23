239443e38298bcd8bfa221197b25c16cd7ea6600
Olga <olgaiv.berlin@gmail.com>
2021-07-23 17:26:13 +0200

    styled the chart

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { useEffect } from 'react';
 import FormComponent from './Components/FormComponent';
 import GlobalStyle from './globalStyles';
 import ChartComponent from './Components/ChartComponent';
-import Test from './Components/TestComponent';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Aos from 'aos';
 import 'aos/dist/aos.css';
@@ -22,9 +21,6 @@ function App() {
         </Route>
         <Route path='/form'>
           <FormComponent />
-        </Route>
-        <Route path='/chart2'>
-          <Test />
         </Route>
         <Route path='/'>
           <FormComponent />
--- src/Components/ChartComponent.js
+++ src/Components/ChartComponent.js
@@ -1,14 +1,16 @@
 import React from 'react';
+import styled from 'styled-components';
+import { FaFacebookSquare } from 'react-icons/fa';
 import {
   ResponsiveContainer,
   LineChart,
   CartesianGrid,
   XAxis,
   YAxis,
-  Label,
   Tooltip,
   Line,
 } from 'recharts';
+
 const data = [
   { name: '0', uv: 0, pv: 0, amt: 0 },
   { name: '0.25', uv: 230, pv: 0, amt: 0 },
@@ -21,6 +23,35 @@ const data = [
   { name: '2', uv: 790, pv: 0, amt: 0 },
 ];
 
+const ChartWrapper = styled.section`
+  display: flex;
+  flex-direction: column;
+  min-height: 50vh;
+  margin: 3rem;
+  padding: 1.2rem;
+  background-color: #f7f9ff;
+  border-radius: 10px;
+  box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
+`;
+
+const TextWrap = styled.div`
+  display: flex;
+  justify-content: ${(props) => props.justifyContent};
+  color: #336581;
+  letter-spacing: 0.05rem;
+  padding: 0.6rem;
+
+  .icon {
+    font-size: 1.5rem;
+    margin-right: 0.5rem;
+  }
+
+  h3 {
+    align-self: center;
+    font-weight: 400;
+  }
+`;
+
 const Chart = () => {
   const onChangeDot = (e, xvalue, yvalue) => {
     console.log(e, xvalue, yvalue);
@@ -28,44 +59,59 @@ const Chart = () => {
   const handleClick = (e) => {
     console.log(e);
   };
+
   return (
-    <ResponsiveContainer width={'100%'} height={400}>
-      <LineChart
-        width={600}
-        height={400}
-        data={data}
-        margin={{ top: 30, right: 30, left: 30, bottom: 30 }}
-      >
-        <Tooltip />
-        <CartesianGrid stroke='#ccc' />
-        <XAxis dataKey='name' domain={['dataMin', 'dataMax']}>
-          <Label
-            value={'Fraction of actual spend'}
-            position='bottom'
-            style={{ textAnchor: 'middle' }}
-          />
-        </XAxis>
-        <YAxis>
-          <Label
-            // value={"Temperature"}
-            position='left'
-            axisLine={false}
-            angle={-90}
-            style={{ textAnchor: 'middle' }}
-          />
-        </YAxis>
-        <Line
-          type='monotone'
-          dataKey='uv'
-          stroke='#3F8CB0'
-          strokeWidth={4}
-          draggable={true}
-          activeDot={{
-            onDrag: (event, payload) => console.log(payload.payload),
+    <ChartWrapper
+      data-aos='zoom-in'
+      data-aos-duration='500'
+      data-aos-once='true'
+      data-aos-anchor-placement='center bottom'
+    >
+      <TextWrap>
+        <FaFacebookSquare className='icon' />
+        <h2>Social_facebook</h2>
+      </TextWrap>
+      <ResponsiveContainer width={'100%'} height={400}>
+        <LineChart
+          width={600}
+          height={400}
+          data={data}
+          margin={{
+            top: 60,
+            right: 60,
+            left: 60,
+            bottom: 20,
           }}
-        />
-      </LineChart>
-    </ResponsiveContainer>
+        >
+          <Tooltip
+            cursor={false}
+            contentStyle={{
+              backgroundColor: 'white',
+              color: '#336581',
+            }}
+          />
+          <CartesianGrid stroke='#ccc' horizontal='true' vertical='' />
+          <XAxis dataKey='name' domain={['dataMin', 'dataMax']}></XAxis>
+          <YAxis></YAxis>
+          <Line
+            type='monotone'
+            dot={false}
+            dataKey='uv'
+            stroke='#336581'
+            strokeWidth={2}
+            draggable={true}
+            activeDot={{
+              onDrag: (event, payload) => console.log(payload.payload),
+              strokeWidth: 2,
+              r: 10,
+            }}
+          />
+        </LineChart>
+      </ResponsiveContainer>
+      <TextWrap justifyContent='center'>
+        <h3>Fraction of actual spend</h3>
+      </TextWrap>
+    </ChartWrapper>
   );
 };
 
